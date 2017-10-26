9e1dbae8f83b31622e4b2e1acc9eb03fb0c02f48
aganpython <947479255@qq.com>
2017-10-26 22:16:58 +0800

    test commit

---
+++ python/Class&Object/T_super.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def display(self):
+        print(self.name, self.age)
+
+class Manager(Person):
+    def __init__(self):
+        super().__init__('agan', 30)
+    def m_display(self):
+        super().display()
+        print('先父类的，再子类的')
+
+m = Manager()
+m.m_display()
